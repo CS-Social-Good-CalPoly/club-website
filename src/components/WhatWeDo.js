9791d0db95808edaf1120eb173a56ea22dae54e5
srirag-vuppala <vuppala@calpoly.edu>
2021-01-23 13:54:12 -0800

    blah

--- src/components/WhatWeDo.js
+++ src/components/WhatWeDo.js
@@ -5,6 +5,7 @@ export default function WhatWeDo() {
     return (
         <div>
             <h3>Home hello! This is what we do</h3>
+            <h2>hello my name is srirag this is for the demo</h2>
         </div>
     )
 }
