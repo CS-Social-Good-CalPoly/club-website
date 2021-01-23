0f1313a34b455d642f14d116a1b01716dba02920
srirag-vuppala <vuppala@calpoly.edu>
2021-01-22 18:57:38 -0800

    Fixed the names of headings and also refactored

--- src/components/Team.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Team() {
-    return (
-        <div>
-            <h3>Hello team</h3>
-        </div>
-    )
-}
---
+++ src/components/WhatWeDo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../assets/WhatWeDo.css';
+
+export default function WhatWeDo() {
+    return (
+        <div>
+            <h3>Home hello! This is what we do</h3>
+        </div>
+    )
+}
