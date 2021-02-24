5398cfab2f778713ca5f54034e52c26ff1bae6ab
Anushree Parmar <anuaparmar@gmail.com>
2021-02-23 19:01:56 -0800

    Added props

--- src/components/OfficerCard.js
+++ src/components/OfficerCard.js
@@ -3,21 +3,20 @@ import '../assets/OfficerCard.css';
 import { Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-function OfficerCard() {
+function OfficerCard(props) {
     return (
         <div className='card' style={ { borderBottom: 'none' } }>
             <Card style={{ width: '15rem', border: 'none' }}>
                 {/* open a new page */}
-                {/* <Card.Link target='_blank' href={'https://www.linkedin.com'}> */}
+                {/* <Card.Link target='_blank' href={props.link}> */}
                 {/* redirect current page */}
-                <Card.Link href={'https://www.linkedin.com'}>
-                    <Card.Img id='officer-pic' variant='top' src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'/>
+                <Card.Link href={props.link}>
+                    <Card.Img id='officer-pic' variant='top' src={props.url}/>
                 </Card.Link>
                 <Card.Body>
-                    <Card.Title id='name'>Officer Name</Card.Title>
-                    <Card.Subtitle id='position'>Officer Position</Card.Subtitle>
-                    <Card.Text id='email'>Email</Card.Text>
+                    <Card.Title id='name'>{props.name}</Card.Title>
+                    <Card.Subtitle id='position'>{props.title}</Card.Subtitle>
+                    <Card.Text id='email'>{props.email}</Card.Text>
                 </Card.Body>
             </Card>
         </div>
