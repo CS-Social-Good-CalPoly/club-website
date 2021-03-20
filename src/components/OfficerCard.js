2c56c92e713519dbc5a3d069944536d25de30f3e
srirag-vuppala <vuppala@calpoly.edu>
2021-03-20 13:44:08 -0700

    clean up

--- src/components/OfficerCard.js
+++ src/components/OfficerCard.js
@@ -3,7 +3,6 @@ import '../assets/OfficerCard.css';
 import { Card } from 'react-bootstrap';
 import { Image } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import imager from '../images/CSSG.png';
 
 function OfficerCard(props) {
     /**
@@ -15,23 +14,21 @@ function OfficerCard(props) {
      *  email: Email address
      *  link: Linkedin URL
      */
-
-
     return (
         <>
-        <div className='card' style={ { borderBottom: 'none' } }>
-            <Card style={{ width: '15rem', border: 'none' }}>
-                <Card.Link href={props.link}>
-                    {/* Need to figure out a way to overlay the linkedin logo on the image */}
-                    <Card.Img as={Image} id='officer-pic' variant="top" fluid={true} src={props.url} alt={props.alt} rounded/>
-                </Card.Link>
-                <Card.Body>
-                    <Card.Title id='name'>{props.name}</Card.Title>
-                    <Card.Subtitle id='position'>{props.title}</Card.Subtitle>
-                    <Card.Text id='email'>{props.email}</Card.Text>
-                </Card.Body>
-            </Card>
-        </div>
+            <div className='card' style={ { borderBottom: 'none' } }>
+                <Card style={{ width: '15rem', border: 'none' }}>
+                    <Card.Link href={props.link}>
+                        {/* Need to figure out a way to overlay the linkedin logo on the image */}
+                        <Card.Img as={Image} id='officer-pic' variant="top" fluid={true} src={props.url} alt={props.alt} rounded/>
+                    </Card.Link>
+                    <Card.Body>
+                        <Card.Title id='name'>{props.name}</Card.Title>
+                        <Card.Subtitle id='position'>{props.title}</Card.Subtitle>
+                        <Card.Text id='email'>{props.email}</Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
         </>
     );
 }
