c66aacd2825830caf36fee1a474c12bd34a0c366
srirag-vuppala <vuppala@calpoly.edu>
2021-03-20 16:41:32 -0700

    Fix the size color and alignment

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,6 +2,13 @@ import React from 'react';
 import '../assets/ProjectCard.css';
 
 const ProjectCard = ({title, techStack, url, description}) => {
+  /**
+   * Props: 
+   *  title: Title of the card
+   *  techStack: the tech stack used for the project
+   *  url: Image URL for the projects
+   *  description: A brief description of the project
+   */
   return (
     <div className="card">
       <img src={url} alt="project"/>
