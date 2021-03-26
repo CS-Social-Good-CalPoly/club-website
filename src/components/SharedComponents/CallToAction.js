b736eb07a51a6a643d61de02a2e41f65a6686a86
jsmcallister98 <jakemcallister@outlook.com>
2021-03-26 16:19:26 -0700

    call to action component

--- src/components/SharedComponents/CallToAction.js
+++ src/components/SharedComponents/CallToAction.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import '../../assets/SharedAssets/CallToAction.css'
+import FilledButton from './FilledButton'
 
-const CallToAction = () => {
+const CallToAction = ({ title, text, buttonUrl, buttonText }) => {
     return (
-        <div>
-            
+        <div className="call-to-action">
+            <h2>{title}</h2>
+            <div className="cta-text">
+                <p>{text}</p>
+            </div>
+            <FilledButton url={buttonUrl} text={buttonText}/>
         </div>
     )
 }
