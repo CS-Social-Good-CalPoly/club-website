c6d12cf7a33da8e33a1194bf4d7d6d53b86dac62
Anushree Parmar <anuaparmar@gmail.com>
2021-03-26 17:33:16 -0700

    Background shapes

--- src/components/SharedComponents/BackgroundShapes.js
+++ src/components/SharedComponents/BackgroundShapes.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import backgroundShapes from './../../images/background-shapes.svg'
 
 const BackgroundShapes = () => {
     return (
-        <div>
-            
+        <div style={{height: '150px'}, {overflow: 'hidden'}} className='waves'>
+            <img src={backgroundShapes} alt='background-shapes' width='100%' />
         </div>
     )
 }
