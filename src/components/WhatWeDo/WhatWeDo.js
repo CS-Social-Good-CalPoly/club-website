3546d75662d5e2d0798b2176f48222bb1b079708
Adelle Vo <adelle.vo@gmail.com>
2021-03-26 23:34:34 -0700

    media query issue

--- src/components/WhatWeDo/WhatWeDo.js
+++ src/components/WhatWeDo/WhatWeDo.js
@@ -4,27 +4,15 @@ import '../../assets/WhatWeDo/WhatWeDo.css';
 export default function WhatWeDo() {
     return (
         <div>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-            <h3>Home hello! This is what we do</h3>
-
+            <h1>Welcome to Cal Poly's</h1>
+            <h1>CS + Social Good</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing 
+                elit. Ultrices ipsum, varius congue enim, neque. 
+                Tincidunt nisl sit quisque nibh consequat tempor, 
+                tortor ultricies. Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit. Ultrices.
+            </p>
         </div>
     )
 }
