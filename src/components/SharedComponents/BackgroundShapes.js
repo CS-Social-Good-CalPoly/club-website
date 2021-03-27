6cf7a06ecbcd76c66cdd3f7b71ca10183ee47c4f
Anushree Parmar <anuaparmar@gmail.com>
2021-03-26 19:31:07 -0700

    Adjusted sizings

--- src/components/SharedComponents/BackgroundShapes.js
+++ src/components/SharedComponents/BackgroundShapes.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import backgroundShapes from './../../images/background-shapes.svg'
+import { useMediaQuery } from 'react-responsive'
+import desktop from './../../images/background-shapes-desktop.svg'
+import mobile from './../../images/background-shapes-mobile.svg'
 
 const BackgroundShapes = () => {
+	const isDesktop = useMediaQuery({ minWidth: 641 })
+
     return (
-        <div style={{height: '150px'}, {overflow: 'hidden'}} className='waves'>
-            <img src={backgroundShapes} alt='background-shapes' width='100%' />
+        <div style={ { height: '100%', overflow: 'hidden' } }>
+			{isDesktop && <img src={desktop} alt='background-shapes' width='100%'/>}
+			{!isDesktop && <img src={mobile} alt='background-shapes' width='100%' />}
         </div>
     )
 }
--- src/components/SharedComponents/Footer.js
+++ src/components/SharedComponents/Footer.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import Waves from './../SharedComponents/Waves.js';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='footer-container'>
+            <Waves />
         </div>
     )
 }
