fad5ed935b532fc83b7f794ba5cd222074d23562
srirag-vuppala <vuppala@calpoly.edu>
2021-03-26 19:59:55 -0700

    removed some testing stuff

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Contact from './components/ContactUs/Contact';
 import Officers from './components/Officers/Officers';
 import Calendar from './components/Calendar/Calendar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
-import BackgroundShapes from './components/SharedComponents/BackgroundShapes';
 
 
 
@@ -18,7 +17,6 @@ function App() {
         <Nav />
         <Switch>
           <Route exact path="/Officers" component={Officers}/>
-          <Route exact path="/Background" component={BackgroundShapes}/>
           <Route exact path="/Calendar" component={Calendar}/>
           <Route exact path="/Projects" component={Projects}/>
           <Route exact path="/Contact" component={Contact}/>
