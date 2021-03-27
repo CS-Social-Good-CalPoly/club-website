d04b615c57cabd3b3dc157c26af59655fc835c4d
Adelle Vo <adelle.vo@gmail.com>
2021-03-27 14:44:40 -0700

    blue-wave-block

--- src/components/WhatWeDo/BlueWaveBlock.js
+++ src/components/WhatWeDo/BlueWaveBlock.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import '../../assets/WhatWeDo/BlueWaveBlock.css'
+import CallToAction from "../SharedComponents/CallToAction"
+import Waves from "../SharedComponents/Waves"
 
-const BlueWaveBlock = () => {
+const BlueWaveBlock = ({ imageUrl, title, text, buttonUrl, buttonText, buttonStyle}) => {
     return (
-        <div>
-            
+        <div className="bluewaveblock" >
+            <div className="bluewaveblock-img">
+                <img src={imageUrl} alt=""/>
+            </div>
+            <div className="bluewaveblock-cta">
+                <CallToAction 
+                    title={title} 
+                    text={text} 
+                    buttonUrl={buttonUrl} 
+                    buttonText={buttonText} 
+                    buttonStyle={buttonStyle}
+                />
+            </div>
+            <div className="bluewaveblock-wave">
+                <Waves 
+                    color='light-blue'
+                />
+            </div>
         </div>
     )
 }
--- src/components/WhatWeDo/WhatWeDo.js
+++ src/components/WhatWeDo/WhatWeDo.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import '../../assets/WhatWeDo/WhatWeDo.css';
 import SubscriptionForm from '../SharedComponents/SubscriptionForm'
+import '../../assets/Projects/Projects.css';
+import default_photo from '../../images/light-blue-wave.svg';
+import project_photo from '../../images/project-team-photo.png';
+import BlueWaveBlock from './BlueWaveBlock';
 
 export default function WhatWeDo() {
     return (
-        <div>
-            <h1>Welcome to Cal Poly's</h1>
-            <h1>CS + Social Good</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing 
-                elit. Ultrices ipsum, varius congue enim, neque. 
-                Tincidunt nisl sit quisque nibh consequat tempor, 
-                tortor ultricies. Lorem ipsum dolor sit amet, 
-                consectetur adipiscing elit. Ultrices.
-            </p>
+        <div>      
+            <BlueWaveBlock
+                imageUrl={project_photo} 
+                title="Join a project team" 
+                text="Lorem ipsum dolor sit amet, consectetur 
+                adipiscing elit. Ultrices ipsum, varius congue enim, 
+                neque. Tincidunt nisl sit quisque nibh consequat 
+                tempor, tortor ultricies." 
+                buttonUrl={default_photo} 
+                buttonText="Learn more" 
+                buttonStyle={"filled"} 
+            />
         </div>
     )
 }
