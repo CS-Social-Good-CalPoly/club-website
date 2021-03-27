951b03fae1db2c7c02e0a339ff1cb66810233361
Anushree Parmar <anuaparmar@gmail.com>
2021-03-26 17:18:58 -0700

    Refactored code

--- src/components/SharedComponents/Waves.js
+++ src/components/SharedComponents/Waves.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import darkBlue from './../../images/dark-blue-wave.svg';
+import lightBlue from './../../images/light-blue-wave.svg';
+import footerDarkBlue from './../../images/footer-dark-blue-wave.svg';
 
-const Waves = () => {
+/*
+ * props
+ * color: wave color ('dark-blue', 'light-blue', 'footer-dark-blue')
+ */
+
+const Waves = (props) => {
+    const color = props.color;
+    let wave;
+    if (color === 'dark-blue') wave = darkBlue;
+    else if (color === 'light-blue') wave = lightBlue;
+    else wave = footerDarkBlue;
+
     return (
-        <div>
-            
-        </div>
+        <div style={{height: '150px'}, {overflow: 'hidden'}} className='waves'>
+            <img src={wave} alt={color} width='100%' />
+        </div>  
     )
 }
 
