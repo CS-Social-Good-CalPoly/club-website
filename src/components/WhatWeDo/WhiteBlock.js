cdd186496e4e0036f9f811d8c1ece3058977defa
jsmcallister98 <jakemcallister@outlook.com>
2021-03-26 22:03:39 -0700

    whiteblock component

--- src/components/WhatWeDo/WhiteBlock.js
+++ src/components/WhatWeDo/WhiteBlock.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import '../../assets/WhatWeDo/WhiteBlock.css'
+import CallToAction from "../SharedComponents/CallToAction"
 
-const WhiteBlock = () => {
+const WhiteBlock = ({ imageUrl, title, text, buttonUrl, buttonText }) => {
     return (
-        <div>
-            
+        <div className="whiteblock" >
+            <div className="whiteblock-cta">
+                <CallToAction 
+                    title={title} 
+                    text={text} 
+                    buttonUrl={buttonUrl} 
+                    buttonText={buttonText} 
+                />
+            </div>
+            <div className="whiteblock-img">
+                <img src={imageUrl} alt=""/>
+            </div>
         </div>
     )
 }
