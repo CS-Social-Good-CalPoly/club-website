1b31768ee7d9074e2a74007b4c5803148c0520fd
Adelle Vo <adelle.vo@gmail.com>
2021-07-02 18:00:54 -0700

    added logo and made some minor changes to match figma

--- src/components/Officers/OfficerCard.js
+++ src/components/Officers/OfficerCard.js
@@ -2,32 +2,29 @@ import React from 'react'
 import '../../assets/Officers/OfficerCard.css';
 import { Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import linkedin_logo from '../../images/linkedin-logo.svg';
 
 function OfficerCard(props) {
     /**
      * props
-     *  url: image url
+     *  photo: officer's profile pic
      *  alt: alternative text for image
-     *  name: Name of Person
-     *  title: Title within club
-     *  email: Email address
-     *  link: Linkedin URL
+     *  name: officer's name
+     *  title: officer's role
+     *  email: email address
+     *  link: LinkedIn URL
      */
     return (
         <>
             <div className='officer-card' style={ { borderBottom: 'none' } }>
                 <Card style={{ width: '13rem', border: 'none' }}>
-                {/* <Card style={{ border: 'none' }}> */}
                     <Card.Link href={props.link}>
-                        {/* Need to figure out a way to overlay the linkedin logo on the image */}
-                        {/* <Card.Img as={Image} id='officer-pic' variant="top" fluid={true} src={props.url} alt={props.alt} rounded/> */}
-                        <Card.Img id='officer-pic' variant='top' src={props.url} alt={props.alt} />
+                        <Card.Img id='officer-pic' variant='top' src={props.photo} alt={props.alt}/>
+                        <Card.Img id='linkedin-logo' src={linkedin_logo}/>
                     </Card.Link>
-                    <Card.Body>
-                        <Card.Title id='name'>{props.name}</Card.Title>
-                        <Card.Subtitle id='position'>{props.title}</Card.Subtitle>
-                        <Card.Text id='email'>{props.email}</Card.Text>
-                    </Card.Body>
+                    <Card.Title id='name'>{props.name}</Card.Title>
+                    <Card.Subtitle id='position'>{props.title}</Card.Subtitle>
+                    <Card.Text id='email'>{props.email}</Card.Text>
                 </Card>
             </div>
         </>
--- src/components/Officers/Officers.js
+++ src/components/Officers/Officers.js
@@ -3,12 +3,18 @@ import '../../assets/Officers/Officers.css';
 import OfficerCard from './OfficerCard';
 import srirag_photo from '../../images/srirag.jpg';
 
-
 const Officers = () => {
     return (
         <div>
             <h3>Part - 1</h3>
-            <OfficerCard link='www.example.com' url= {srirag_photo} name="Srirag Vuppala" title="Community Officer" email="@gmail.com" alt="Photo of Srirag" />
+            <OfficerCard 
+                link="example.com"
+                photo={srirag_photo} 
+                name="Srirag Vuppala" 
+                title="Community Officer" 
+                email="@gmail.com" 
+                alt="Photo of Srirag" 
+            />
         </div>
     )
 }
