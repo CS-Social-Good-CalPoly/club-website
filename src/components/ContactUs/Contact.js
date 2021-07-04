353b974853fa5e6fb8c1863d3614f3c1964e2bf9
Adelle Vo <adelle.vo@gmail.com>
2021-07-03 23:01:00 -0700

    contact us banner component

--- src/components/ContactUs/Contact.js
+++ src/components/ContactUs/Contact.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import '../../assets/ContactUs/Contact.css';
+import { ContactUsBanner } from './ContactUsBanner';
 
-
-
-export default function Contact() {
+const Contact = () => {
     return (
         <div>
             <h3>Contact us</h3>
         </div>
     )
 }
+
+const Banner = () => {
+    return (
+        <ContactUsBanner></ContactUsBanner>
+    )
+}
+
+const final = () => {
+    return(
+        <>
+        <Contact />
+        <Banner />
+        </>
+    ) 
+}
+
+
+export default final;
---
+++ src/components/ContactUs/ContactUsBanner.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../../assets/ContactUs/ContactUsBanner.css'
+import CallToAction from "../SharedComponents/CallToAction"
+import Waves from "../SharedComponents/Waves"
+import linkedin from "../../images/linkedin.svg"
+import discord from "../../images/discord.svg"
+import facebook from "../../images/facebook.svg"
+import slack from "../../images/slack.svg"
+
+export const ContactUsBanner = () => {
+    return (
+        <div className="contactusbanner-container">
+            <h3>Connect with us on social media!</h3>
+            <div className="socialmedia-parent">              
+                <a href="#" className="socialmedia-child">
+                    <img src={linkedin}/>
+                </a>
+                <a href="#" className="socialmedia-child">
+                    <img src={discord}/>
+                </a> 
+                <a href="#" className="socialmedia-child">
+                    <img src={facebook}/>
+                </a> 
+                <a href="#" className="socialmedia-child">
+                    <img src={slack}/>
+                </a> 
+            </div>
+        </div>
+    )
+};
+
