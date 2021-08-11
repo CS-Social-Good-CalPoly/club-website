224a6cded8de578d3fd835e11de8055a6bbb7dda
Adelle Vo <adelle.vo@gmail.com>
2021-08-10 20:38:31 -1000

    created tech shift alliance component

--- src/components/WhatWeDo/Techshift.js
+++ src/components/WhatWeDo/Techshift.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import '../../assets/WhatWeDo/Techshift.css'
 
-const Techshift = () => {
+const TechShift = ({imageUrl}) => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="tsa">
+                <div className="tsa-left">
+                    <img src={imageUrl} alt=""/>
+                </div>
+                <div className="tsa-right">
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultrices ipsum, varius congue enim, neque. Tincidunt nisl sit quisque nibh consequat tempor, tortor ultricies. tempor, tortor ultricies. tempor, tortor ultricies.
+                    </p> 
+                </div>
+            </div>
+        </>
     )
 }
 
-export default Techshift
+export default TechShift;
--- src/components/WhatWeDo/WhatWeDo.js
+++ src/components/WhatWeDo/WhatWeDo.js
@@ -5,13 +5,19 @@ import '../../assets/Projects/Projects.css';
 /*import default_photo from '../../images/light-blue-wave.svg';*/
 import default_photo from '../../images/gray-wave.svg';
 import project_photo from '../../images/project-team-photo.png';
+import tsa_photo from '../../images/tsa-photo.svg';
 import BlueWaveBlock from './BlueWaveBlock';
+import TechShift from './Techshift';
+import WhatWeDoBanner from './WhatWeDoBanner';
 import BlueNewsletterSubBox from './BlueNewsletterSubBox';
 import Footer from '../SharedComponents/Footer';
 
 export default function WhatWeDo() {
     return (
         <div>      
+            <TechShift
+                imageUrl={tsa_photo}
+            />
             <BlueWaveBlock
                 imageUrl={project_photo} 
                 title="Join a project team" 
