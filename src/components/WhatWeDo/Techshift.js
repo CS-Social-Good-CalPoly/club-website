37d11c1faebcc9ce35f71028b1021cd282002a9a
Adelle Vo <adelle.vo@gmail.com>
2021-08-14 21:49:08 -0700

    text

--- src/components/WhatWeDo/Techshift.js
+++ src/components/WhatWeDo/Techshift.js
@@ -10,6 +10,10 @@ const TechShift = ({imageUrl}) => {
                 </div>
                 <div className="tsa-right">
                     <p>
+                    Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit. Ultrices ipsum, varius congue enim, 
+                    neque. Tincidunt nisl sit quisque nibh consequat 
+                    tempor, tortor ultricies.
                     {/* We are partned with the Tech Shift Alliance, an organization that strives to empower students in using technology to serve our community. */}
                     </p> 
                 </div>
