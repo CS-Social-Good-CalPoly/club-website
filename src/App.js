fbab77d57b9bd7a15c29bbd8bf8f9655a406b3d0
david-1-nguyen <40614920+david-1-nguyen@users.noreply.github.com>
2021-08-14 21:17:21 -0700

    Removed inserted footer from each page and moved to App.js. Removed unused imports from other pages also.

--- src/App.js
+++ src/App.js
@@ -6,23 +6,25 @@ import Nav from './components/SharedComponents/Nav';
 import Contact from './components/ContactUs/Contact';
 import Officers from './components/Officers/Officers';
 import Calendar from './components/Calendar/Calendar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Footer from "./components/SharedComponents/Footer";
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Nav />
-        <Switch>
-          <Route exact path="/Officers" component={Officers}/>
-          <Route exact path="/Calendar" component={Calendar}/>
-          <Route exact path="/Projects" component={Projects}/>
-          <Route exact path="/Contact" component={Contact}/>
-          <Route path="/" component={WhatWeDo}/>
-        </Switch>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Nav/>
+                <Switch>
+                    <Route path="/" component={WhatWeDo}/>
+                    <Route exact path="/Officers" component={Officers}/>
+                    <Route exact path="/Calendar" component={Calendar}/>
+                    <Route exact path="/Projects" component={Projects}/>
+                    <Route exact path="/Contact" component={Contact}/>
+                </Switch>
+                <Footer/>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
