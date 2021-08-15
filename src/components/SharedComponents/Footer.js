8aae3c578cef4ebac42b46346db90b7a409a8aac
Adelle Vo <adelle.vo@gmail.com>
2021-08-14 22:04:26 -0700

    new icons

--- src/components/SharedComponents/Footer.js
+++ src/components/SharedComponents/Footer.js
@@ -8,24 +8,28 @@ import slack from "../../images/slack.svg"
 const Footer = () => {
     return (
         <div className="footer-container">
-            <div className="footer-content">
-                <h3>Connect with us on social media!</h3>
-                <div className="socialmedia-parent">
-                    <a href="#" className="socialmedia-child">
-                        <img src={linkedin}/>
-                    </a>
-                    <a href="#" className="socialmedia-child">
-                        <img src={discord}/>
-                    </a>
-                    <a href="https://www.facebook.com/CSSGCalPoly/" className="socialmedia-child">
-                        <img src={facebook}/>
-                    </a>
-                    <a href="#" className="socialmedia-child">
-                        <img src={slack}/>
-                    </a>
-                </div>
-                <p>© 2021 Cal Poly SLO CS + Social Good.</p>
+            <h3>Connect with us on social media!</h3>
+            <div className="socialmedia-parent">              
+                <a href="#" className="socialmedia-child">
+                    <img src={linkedin}/>
+                </a>
+                <a href="#" className="socialmedia-child">
+                    <img src={discord}/>
+                </a> 
+                <a href="https://www.facebook.com/CSSGCalPoly/" className="socialmedia-child">
+                    <img src={facebook}/>
+                </a> 
+                <a href="#" className="socialmedia-child">
+                    <img src={slack}/>
+                </a> 
+                <a href="#" className="socialmedia-child">
+                    <img src={instagram}/>
+                </a>
+                <a href="#" className="socialmedia-child">
+                    <img src={linktree}/>
+                </a>
             </div>
+            <p>© 2021 Cal Poly SLO CS + Social Good.</p>
         </div>
     )
 }
