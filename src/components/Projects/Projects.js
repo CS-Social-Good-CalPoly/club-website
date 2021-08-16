6f763fb939bb6fc8aa0ed96b3f9603676483dbf1
Adelle Vo <adelle.vo@gmail.com>
2021-08-16 10:42:59 -0700

    project layout

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import ProjectBanner from './ProjectBanner.js';
-import ProjectCard from './ProjectCard.js';
+// import ProjectCard from './ProjectCard.js';
+import ProjectLayout from './ProjectLayout.js';
 import '../../assets/Projects/Projects.css';
-import default_photo from '../../images/project-card-default.png';
+import photo from '../../images/project-card-default.png';
 import Footer from '../SharedComponents/Footer';
 
 export default function Projects() {
     return (
         <div>
             <ProjectBanner/>
-            <ProjectCard 
-                title="Vera" 
-                //techStack="MongoDB, Express.js, React and Node.js" 
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultrices ipsum, varius congue enim, neque. Tincidunt nisl sit quisque nibh consequat tempor, tortor ultricies." 
-                url={default_photo} 
-                openApp="Applications open September 20, 2021"
-            />
+            <ProjectLayout/>
             <Footer/>
         </div>
     )
     
-}
+};
