dfb2c96f572ad0410911bffefcd55f0a18d5d1a8
david-1-nguyen <40614920+david-1-nguyen@users.noreply.github.com>
2021-08-17 13:19:23 -0700

    Added padding to be less of an eyesore and vertically aligned.

--- src/components/SharedComponents/Footer.js
+++ src/components/SharedComponents/Footer.js
@@ -8,22 +8,24 @@ import slack from "../../images/slack.svg"
 const Footer = () => {
     return (
         <div className="footer-container">
-            <h3>Connect with us on social media!</h3>
-            <div className="socialmedia-parent">              
-                <a href="#" className="socialmedia-child">
-                    <img src={linkedin}/>
-                </a>
-                <a href="#" className="socialmedia-child">
-                    <img src={discord}/>
-                </a> 
-                <a href="https://www.facebook.com/CSSGCalPoly/" className="socialmedia-child">
-                    <img src={facebook}/>
-                </a> 
-                <a href="#" className="socialmedia-child">
-                    <img src={slack}/>
-                </a> 
+            <div className="footer-content">
+                <h3>Connect with us on social media!</h3>
+                <div className="socialmedia-parent">
+                    <a href="#" className="socialmedia-child">
+                        <img src={linkedin}/>
+                    </a>
+                    <a href="#" className="socialmedia-child">
+                        <img src={discord}/>
+                    </a>
+                    <a href="https://www.facebook.com/CSSGCalPoly/" className="socialmedia-child">
+                        <img src={facebook}/>
+                    </a>
+                    <a href="#" className="socialmedia-child">
+                        <img src={slack}/>
+                    </a>
+                </div>
+                <p>© 2021 Cal Poly SLO CS + Social Good.</p>
             </div>
-            <p>© 2021 Cal Poly SLO CS + Social Good.</p>
         </div>
     )
 }
