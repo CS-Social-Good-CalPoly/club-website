8db6cab2f11af477406280c991606894a6618616
david-1-nguyen <40614920+david-1-nguyen@users.noreply.github.com>
2021-08-17 13:28:25 -0700

    Compatibility changes for master branch

--- src/components/SharedComponents/Footer.js
+++ src/components/SharedComponents/Footer.js
@@ -11,9 +11,9 @@ const Footer = () => {
             <div className="footer-content">
                 <h3>Connect with us on social media!</h3>
                 <div className="socialmedia-parent">
-                    <a href="#" className="socialmedia-child">
-                        <img src={linkedin}/>
-                    </a>
+                    {/* <a href="#" className="socialmedia-child">
+                    <img src={linkedin}/>
+                </a> */}
                     <a href="#" className="socialmedia-child">
                         <img src={discord}/>
                     </a>
@@ -23,31 +23,15 @@ const Footer = () => {
                     <a href="#" className="socialmedia-child">
                         <img src={slack}/>
                     </a>
+                    <a href="https://www.instagram.com/calpoly.cssg/" className="socialmedia-child">
+                        <img src={instagram}/>
+                    </a>
+                    <a href="#" className="socialmedia-child">
+                        <img src={linktree}/>
+                    </a>
                 </div>
                 <p>© 2021 Cal Poly SLO CS + Social Good.</p>
             </div>
-            <h3>Connect with us on social media!</h3>
-            <div className="socialmedia-parent">
-                {/* <a href="#" className="socialmedia-child">
-                    <img src={linkedin}/>
-                </a> */}
-                <a href="#" className="socialmedia-child">
-                    <img src={discord}/>
-                </a>
-                <a href="https://www.facebook.com/CSSGCalPoly/" className="socialmedia-child">
-                    <img src={facebook}/>
-                </a>
-                <a href="#" className="socialmedia-child">
-                    <img src={slack}/>
-                </a>
-                <a href="https://www.instagram.com/calpoly.cssg/" className="socialmedia-child">
-                    <img src={instagram}/>
-                </a>
-                <a href="#" className="socialmedia-child">
-                    <img src={linktree}/>
-                </a>
-            </div>
-            <p>© 2021 Cal Poly SLO CS + Social Good.</p>
         </div>
     )
 }
