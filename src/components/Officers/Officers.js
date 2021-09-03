68bd1c54b63802e2c9a30d2f77313f81e53e19bc
david-1-nguyen <40614920+david-1-nguyen@users.noreply.github.com>
2021-09-02 18:58:55 -0700

    Updated officer names for now on officers. Added cssg logo for placeholder image until we get all officer images uploaded.

--- src/components/Officers/Officers.js
+++ src/components/Officers/Officers.js
@@ -2,78 +2,77 @@ import React from 'react'
 import '../../assets/Officers/Officers.css';
 import OfficerBanner from './OfficerBanner';
 import OfficerLayout from './OfficerLayout';
-import srirag_photo from "../../images/srirag.jpg";
-import '../../assets/Officers/Officers.css';
+import placeholder from "../../images/officer_images/placeholder.png"
 
 export default function Officers() {
     const cards = [
         {
-            link: "example.com",
-            photo: srirag_photo,
+            link: "/",
+            photo: placeholder,
+            name: "David Nguyen",
+            title: "President",
+            email: "dnguy260@calpoly.edu",
+            alt: "Photo of David"
+        },
+        {
+            link: "/",
+            photo: placeholder,
+            name: "Anushree Parmar",
+            title: "Treasurer",
+            email: "aaparmar@calpoly.edu",
+            alt: "Photo of Anushree"
+        },
+        {
+            link: "/",
+            photo: placeholder,
+            name: "Kenny Lau",
+            title: "Tech VP | RE Cares Lead",
+            email: "klau24@calpoly.edu",
+            alt: "Photo of Kenny"
+        },
+        {
+            link: "/",
+            photo: placeholder,
+            name: "Nick Firme",
+            title: "Workshop Lead",
+            email: "nfirme@calpoly.edu",
+            alt: "Photo of Nick"
+        },
+        {
+            link: "/",
+            photo: placeholder,
+            name: "Sean Sng",
+            title: "Newsletter Manager",
+            email: "sksng@calpoly.edu",
+            alt: "Photo of Sean"
+        },
+        {
+            link: "/",
+            photo: placeholder,
+            name: "Ria Bhatt",
+            title: "Social Media Manager",
+            email: "rbhatt01@calpoly.edu",
+            alt: "Photo of Ria"
+        },
+        {
+            link: "/",
+            photo: placeholder,
             name: "Srirag Vuppala",
             title: "Community Officer",
             email: "@gmail.com",
             alt: "Photo of Srirag"
         },
         {
-            link: "example.com",
-            photo: srirag_photo,
+            link: "/",
+            photo: placeholder,
             name: "Srirag Vuppala",
             title: "Community Officer",
             email: "@gmail.com",
             alt: "Photo of Srirag"
         },
         {
-            link: "example.com",
-            photo: srirag_photo,
-            name: "Srirag Vuppala",
-            title: "Community Officer",
-            email: "@gmail.com",
-            alt: "Photo of Srirag"
-        },
-        {
-            link: "example.com",
-            photo: srirag_photo,
-            name: "Srirag Vuppala",
-            title: "Community Officer",
-            email: "@gmail.com",
-            alt: "Photo of Srirag"
-        },
-        {
-            link: "example.com",
-            photo: srirag_photo,
-            name: "Srirag Vuppala",
-            title: "Community Officer",
-            email: "@gmail.com",
-            alt: "Photo of Srirag"
-        },
-        {
-            link: "example.com",
-            photo: srirag_photo,
-            name: "Srirag Vuppala",
-            title: "Community Officer",
-            email: "@gmail.com",
-            alt: "Photo of Srirag"
-        },
-        {
-            link: "example.com",
-            photo: srirag_photo,
-            name: "Srirag Vuppala",
-            title: "Community Officer",
-            email: "@gmail.com",
-            alt: "Photo of Srirag"
-        },
-        {
-            link: "example.com",
-            photo: srirag_photo,
-            name: "Srirag Vuppala",
-            title: "Community Officer",
-            email: "@gmail.com",
-            alt: "Photo of Srirag"
-        },
-        {
-            link: "example.com",
-            photo: srirag_photo,
+            link: "/",
+            photo: placeholder,
             name: "Srirag Vuppala",
             title: "Community Officer",
             email: "@gmail.com",
