5e60298de8447e27a823e58b12a1d9dce06a4d42
Adelle Vo <adelle.vo@gmail.com>
2021-09-10 09:53:35 -0700

    letter spacing + file names

--- src/components/WhatWeDo/WhatWeDo.js
+++ src/components/WhatWeDo/WhatWeDo.js
@@ -5,22 +5,22 @@ import default_photo from '../../images/gray-wave.svg';
 import project_photo from '../../images/project-team-photo.png';
 import whatwedo_photo from '../../images/whiteblock-img.png';
 import tsa_photo from '../../images/tsa-photo.svg';
-import BlueWaveBlock from './BlueWaveBlock';
+import GreyWaveBlock from './GreyWaveBlock';
 import WhiteWaveBlock from './WhiteWaveBlock';
 import TechShift from './Techshift';
-import BlueNewsletterSubBox from './BlueNewsletterSubBox';
+import NewsletterSubBox from './NewsletterSubBox';
 import WhatWeDoBanner from './WhatWeDoBanner';
 // import SubscriptionForm from '../SharedComponents/SubscriptionForm'
 // import default_photo from '../../images/light-blue-wave.svg';
 
 export default function WhatWeDo() {
     return (
-        <div>
+        <div className="wwd">
             <WhatWeDoBanner/>
             <TechShift
                 imageUrl={tsa_photo}
             />
-            <BlueWaveBlock
+            <GreyWaveBlock
                 imageUrl={project_photo}
                 title="Join our team"
                 text="We are currently looking for a Communications VP and a Program and Events Officer to join our 2021-2022 officer team. These two positions are on the social side of our club, so if that appeals to you, don't hesitate to apply!"
@@ -36,7 +36,7 @@ export default function WhatWeDo() {
                 buttonText="View our projects"
                 buttonStyle="filled"
             />
-            <BlueNewsletterSubBox/>
+            <NewsletterSubBox/>
         </div>
     )
 }
